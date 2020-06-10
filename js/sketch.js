7a1941d15ad984962339e4c27c7c19bf19db5454
ayoamdi <chaves.diana98@yahoo.com>
2020-06-10 15:16:13 +0200

    ouais

--- js/sketch.js
+++ js/sketch.js
@@ -1,31 +1,86 @@
 let target;
+let projectiles = [];
+
+
 
 function setup() {
+    colorMode(HSB, 255);
+    background(0, 0, 255);
     createCanvas(windowWidth, windowHeight);
-    target = new Target(random(0, width), random(0, height), 300, true);
-    
+    target = new Target(random(0, width), random(0, height), 300, true, random(255, 0));
+
+    for (let i = 0; i < 1; i += 1) {
+        projectiles[i] = new Projectile(width / 2, height / 2, 20, 20, 0, random(0, 255))
+    }
+
+
+
 }
 
 function draw() {
-    background(255);
+    background(255, 0, 255)
+    // for (let bgColor = 0; bgColor < 255; bgColor = bgColor + random(0, 255)) {
+    //     fill(0, 0, bgColor);
+    //     rect(0, 0, width, height);
+    // }
+
+    //target change de place
     let targetDistance = dist(mouseX, mouseY, target.x, target.y);
-    if (targetDistance < target.size / 2) { 
-        target = new Target(random(0, width), random(0, height), random(20, 150), true);
+    if (targetDistance < target.size / 2) {
+        target = new Target(random(0, width), random(0, height), random(20, 150), true, random(255, 0));
     }
-    console.log(targetDistance);
     target.display();
+
+    //curseur
+    fill(255, 190, 255, 30);
+    ellipse(mouseX, mouseY, 5, 5);
+
+    //projectiles
+    for (let i = 0; i < projectiles.length; i += 1) {
+        projectiles[i].afficher();
+        projectiles[i].bouger();
+    }
+
+    console.log();
+
+
 }
 
 class Target {
-    constructor(_x, _y, _size, _isActive) {
+    constructor(_x, _y, _size, _isActive, _colorTarget) {
         this.x = _x;
         this.y = _y;
         this.size = _size;
         this.isActive = _isActive;
+        this.colorTarget = _colorTarget;
     }
 
     display() {
+        fill(this.colorTarget, 200, 255);
         ellipse(this.x, this.y, this.size);
+    }
+}
+
+class Projectile {
+    constructor(x, y, radius, vitesseX, vitesseY, colorProjectile) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.colorProjectile = colorProjectile;
+
+    }
+
+    afficher() {
+        fill(this.colorProjectile, 200, 255);
+        for (this.x = 0; this.x < width; this.x = this.x + 50) {
+            ellipse(this.x, this.y, this.radius, this.radius);
+        }
+    }
+
+    bouger() {
+        this.x += this.vitesseX;
+        this.y += this.vitesseY;
+
     }
 }
 
