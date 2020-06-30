fd5b50aa83d76918b46c5234a67d7166646f8a79
ayoamdi <chaves.diana98@yahoo.com>
2020-06-30 13:58:37 +0200

    début score

--- js/sketch.js
+++ js/sketch.js
@@ -1,162 +1,173 @@
 let target;
 let projectiles = [];
 let level = 2;
+let score = 0;
 
 function setup() {
-  colorMode(HSB, 255);
-  background(0, 0, 255);
-  createCanvas(windowWidth, windowHeight);
-  target = new Target(
-    random(0, width),
-    random(0, height),
-    300,
-    true,
-    random(255, 0)
-  );
-  generateProjectiles();
+    colorMode(HSB, 255);
+    background(0, 0, 255);
+    createCanvas(windowWidth, windowHeight);
+    target = new Target(
+        random(0, width),
+        random(0, height),
+        300,
+        true,
+        random(255, 0)
+    );
+    generateProjectiles();
 }
 
 function draw() {
-  background(255, 0, 255);
-  // for (let bgColor = 0; bgColor < 255; bgColor = bgColor + random(0, 255)) {
-  //     fill(0, 0, bgColor);
-  //     rect(0, 0, width, height);
-  // }
+    background(255, 255, 255);
+    noStroke();
+    fill(255, 255, 0);
+    textSize(50);
+    text(score, width / 2, 100);
 
-  //target change de place
-  let targetDistance = dist(mouseX, mouseY, target.x, target.y);
+    // for (let bgColor = 0; bgColor < 255; bgColor = bgColor + random(0, 255)) {
+    //     fill(0, 0, bgColor);
+    //     rect(0, 0, width, height);
+    // }
 
-  if (targetDistance < target.size / 2) {
-    target = new Target(
-      random(0, width),
-      random(0, height),
-      random(20, 150),
-      true,
-      random(255, 0)
-    );
+    //target change de place
+    let targetDistance = dist(mouseX, mouseY, target.x, target.y);
 
-    level = level + 10;
+    if (targetDistance < target.size / 2) {
+        target = new Target(
+            random(0, width),
+            random(0, height),
+            random(20, 150),
+            true,
+            random(255, 0)
+        );
 
-    generateProjectiles();
-  }
+        level = level + 10;
 
-  target.display();
+        generateProjectiles();
 
-  //curseur
-  fill(255, 190, 255, 30);
-  ellipse(mouseX, mouseY, 5, 5);
+        score = +1;
 
-  //projectiles
-
-  for (let i = 0; i < projectiles.length; i += 1) {
-    projectiles[i].afficher();
-    projectiles[i].bouger();
-    for (let j = 0; j < projectiles.length; j += 1) {
-      //check l'intersection entre les projectiles
-      if (i != j && projectiles[i].intersection(projectiles[j])) {
-        projectiles[i].rebond();
-        projectiles[j].rebond();
-      }
     }
-  }
+
+    target.display();
+
+    //curseur
+    fill(0, 0, 255);
+    ellipse(mouseX, mouseY, 5, 5);
+
+    //projectiles
+
+    for (let i = 0; i < projectiles.length; i += 1) {
+        projectiles[i].afficher();
+        projectiles[i].bouger();
+        for (let j = 0; j < projectiles.length; j += 1) {
+            //check l'intersection entre les projectiles
+            if (i != j && projectiles[i].intersection(projectiles[j])) {
+                projectiles[i].rebond();
+                projectiles[j].rebond();
+            }
+        }
+    }
 }
 
 function generateProjectiles() {
-  for (let i = 0; i < level; i += 1) {
-    // 0 = Haut;
-    // 1 = Gauche
-    // 2 = Bas
-    // 3 = Droite
-    let r = int(random(0, 4));
+    for (let i = 0; i < level; i += 1) {
+        // 0 = Haut;
+        // 1 = Gauche
+        // 2 = Bas
+        // 3 = Droite
+        let r = int(random(0, 4));
 
-    let startX;
-    let startY;
-    let startSpeedX = random(3, 8);
-    let startSpeedY = random(3, 8);
-    let projectileRadius = random(10, 30);
+        let startX;
+        let startY;
+        let startSpeedX = random(3, 8);
+        let startSpeedY = random(3, 8);
+        let projectileRadius = random(10, 30);
 
-    if (r == 0) {
-      startX = random(0, width);
-      startY = -projectileRadius;
-    } else if (r == 1) {
-      startX = -projectileRadius;
-      startY = random(0, height);
-    } else if (r == 2) {
-      startX = random(0, width);
-      startY = height + projectileRadius;
-      startSpeedY = -startSpeedY;
-    } else if (r == 3) {
-      startX = width + projectileRadius;
-      startY = random(0, height);
-      startSpeedX = -startSpeedX;
+        if (r == 0) {
+            startX = random(0, width);
+            startY = -projectileRadius;
+        } else if (r == 1) {
+            startX = -projectileRadius;
+            startY = random(0, height);
+        } else if (r == 2) {
+            startX = random(0, width);
+            startY = height + projectileRadius;
+            startSpeedY = -startSpeedY;
+        } else if (r == 3) {
+            startX = width + projectileRadius;
+            startY = random(0, height);
+            startSpeedX = -startSpeedX;
+        }
+        projectiles[i] = new Projectile(
+            startX,
+            startY,
+            projectileRadius,
+            startSpeedX,
+            startSpeedY,
+            fill(0, 0, 0)
+        );
+
+        // let di = dist(projectiles[].x, projectiles[].y, projectiles[].x, projectiles[].y, );
+        //
+        // if (di > Projectile.radius / 2) {
+        //     projectiles.afficher
+        // }
     }
-    projectiles[i] = new Projectile(
-      startX,
-      startY,
-      projectileRadius,
-      startSpeedX,
-      startSpeedY,
-      random(0, 255)
-    );
-
-    // let di = dist(projectiles[].x, projectiles[].y, projectiles[].x, projectiles[].y, );
-    //
-    // if (di > Projectile.radius / 2) {
-    //     projectiles.afficher
-    // }
-  }
 }
 
 class Target {
-  constructor(_x, _y, _size, _isActive, _colorTarget) {
-    this.x = _x;
-    this.y = _y;
-    this.size = _size;
-    this.isActive = _isActive;
-    this.colorTarget = _colorTarget;
-  }
+    constructor(_x, _y, _size, _isActive, _colorTarget) {
+        this.x = _x;
+        this.y = _y;
+        this.size = _size;
+        this.isActive = _isActive;
+        this.colorTarget = _colorTarget;
+    }
 
-  display() {
-    fill(this.colorTarget, 200, 255);
-    ellipse(this.x, this.y, this.size);
-  }
+    display() {
+        fill(this.colorTarget, 200, 255);
+        ellipse(this.x, this.y, this.size);
+    }
+
+
 }
 
 class Projectile {
-  constructor(_x, _y, _radius, _vitesseX, _vitesseY, _colorProjectile) {
-    this.x = _x;
-    this.y = _y;
-    this.radius = _radius;
-    this.vitesseX = _vitesseX;
-    this.vitesseY = _vitesseY;
-    this.colorProjectile = _colorProjectile;
-  }
-
-  rebond() {
-    //this.colorProjectile = 0;
-    this.vitesseX = -this.vitesseX;
-    this.vitesseY = -this.vitesseY;
-  }
-
-  intersection(other) {
-    let d = dist(this.x, this.y, other.x, other.y);
-
-    if (d < this.radius + other.radius) {
-      return true;
-    } else if (d > this.radius + other.radius) {
-      return false;
+    constructor(_x, _y, _radius, _vitesseX, _vitesseY, _colorProjectile) {
+        this.x = _x;
+        this.y = _y;
+        this.radius = _radius;
+        this.vitesseX = _vitesseX;
+        this.vitesseY = _vitesseY;
+        this.colorProjectile = _colorProjectile;
     }
-  }
 
-  afficher() {
-    fill(this.colorProjectile, 200, 255);
-    ellipse(this.x, this.y, this.radius, this.radius);
-  }
+    rebond() {
+        //this.colorProjectile = 0;
+        this.vitesseX = -this.vitesseX;
+        this.vitesseY = -this.vitesseY;
+    }
 
-  bouger() {
-    this.x += this.vitesseX;
-    this.y += this.vitesseY;
-  }
+    intersection(other) {
+        let d = dist(this.x, this.y, other.x, other.y);
+
+        if (d < this.radius + other.radius) {
+            return true;
+        } else if (d > this.radius + other.radius) {
+            return false;
+        }
+    }
+
+    afficher() {
+        fill(this.colorProjectile, 200, 255);
+        ellipse(this.x, this.y, this.radius, this.radius);
+    }
+
+    bouger() {
+        this.x += this.vitesseX;
+        this.y += this.vitesseY;
+    }
 }
 
 // let projectiles = [];
